2aed3da3bd1ab4f79818ecfae5e8a43f661c3e4f
munafahad <alfahadmuna@gmail.com>
2016-04-09 16:37:46 -0700

    adding .java files

---
+++ src/bufmgr/Clock.java
@@ -0,0 +1,17 @@
+package bufmgr;
+
+import global.GlobalConst;
+import global.Minibase;
+import global.Page;
+import global.PageId;
+
+import java.util.HashMap;
+
+public class Clock implements GlobalConst {
+
+	public Clock() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
---
+++ src/bufmgr/FrameDesc.java
@@ -0,0 +1,31 @@
+package bufmgr;
+
+import global.PageId;
+
+public class FrameDesc {
+
+	
+	/*
+		* Each frame has certain states associated with it. These states include whether the frame
+		is dirty, whether it includes valid data (data which reflects data in a disk page), and if it
+		includes valid data then what is the disk page number of the data, how many callers
+		have pins on the data (the pin count), and any other informa>on you wish to store, for
+		example informa>on relevant to the replacement algorithm. Be sure to store this informa
+		>on as efficiently as possible while preserving readability
+	 */
+	
+	protected boolean dirty;
+	protected boolean valid; //valid data in frame or not
+	protected PageId pageno; //disk page number
+	protected int pin_count; // # of callers who pin data
+	// reference bit?
+	
+	public FrameDesc() {
+		dirty = false;
+		valid = false; 
+		// pageno = 
+		pin_count = 0;
+		
+	}
+
+}
