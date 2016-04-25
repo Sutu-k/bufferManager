4b5eff51b6df7b947ab5af80a1ba859341d90913
Colleen Toth <tothcol@pdx.edu>
2016-04-24 17:10:27 -0700

    modified clock to include a current and loop twice through the hash table

--- src/bufmgr/Clock.java
+++ src/bufmgr/Clock.java
@@ -3,7 +3,6 @@ package bufmgr;
 import global.GlobalConst;
 import global.PageId;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -12,36 +11,44 @@ import java.util.Map;
 public class Clock implements GlobalConst {
 
 	protected HashMap<PageId, FrameDesc> pageFrameMap;
+	protected FrameDesc current;
 	 
 	public Clock(BufMgr bm) {
 
 		this.pageFrameMap = bm.pageFrameMap;
+		this.current = null;
 	}
 	
 	
 	public FrameDesc pickVictim() {
-		
+
+		int count = 0;
 		//(frametab.length)*2: as we need to check the buff. pool 2 times
-		Iterator map = pageFrameMap.entrySet().iterator();
-		while (map.hasNext()) {
-			Map.Entry pair = (Map.Entry) map.next();
-			PageId key = (PageId) pair.getKey();
-			FrameDesc frame = (FrameDesc) pair.getValue();
-			
-			//1. if data in bufpool[current] is not valid, choose current
-			if (!frame.valid){
-				return frame;
-			}
-			//2. if frametab[current]'s pin count is 0
-			else if (frame.pin_count == 0){
-				// check if frametab [current] has refbit
-				if (frame.refbit){
-					frame.refbit = false;
-				} else {
+		while (count < 2) {
+			Iterator map = pageFrameMap.entrySet().iterator();
+			while (map.hasNext()) {
+				Map.Entry pair = (Map.Entry) map.next();
+				PageId key = (PageId) pair.getKey();
+				FrameDesc frame = (FrameDesc) pair.getValue();
+
+				//1. if data in bufpool[current] is not valid, choose current
+				if (!frame.valid) {
+					current = frame;
 					return frame;
-				}	
+				}
+				//2. if frametab[current]'s pin count is 0
+				else if (frame.pin_count == 0) {
+					// check if frametab [current] has refbit
+					if (frame.refbit) {
+						frame.refbit = false;
+					} else {
+						current = frame;
+						return frame;
+					}
+				}
+				// increment current, mod
+				count++;
 			}
-			// increment current, mod
 		}
 		
 		// (-1) if No frame available in the buff. pool
